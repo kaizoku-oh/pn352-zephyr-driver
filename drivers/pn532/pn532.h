c56b88af924acf1bbba719a45c1f9ba0692d4ee0
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-09 18:09:04 +0100

    Update driver structure

---
+++ drivers/pn532/pn532.h
@@ -0,0 +1,90 @@
+#ifndef PN532_H
+#define PN532_H
+
+#include <zephyr/device.h>
+#include <zephyr/toolchain.h>
+
+/**
+ * @defgroup drivers_pn532 PN532 drivers
+ * @ingroup drivers
+ * @{
+ *
+ * @brief A custom driver class to interface with the PN532 NFC module
+ *
+ * This driver provides an interface to the PN532
+ * NFC controller over I2C. It allows applications to communicate
+ *
+ * It exposes a custom device driver API built using Zephyr's __subsystem and
+ * z_impl_ mechanism, enabling structured and extendable access to PN532 features.
+ */
+
+/**
+ * @defgroup drivers_pn532_ops PN532 driver operations
+ * @{
+ *
+ * @brief Operations of the PN532 driver class.
+ *
+ * Each driver class tipically provides a set of operations that need to be
+ * implemented by each driver. These are used to implement the public API. If
+ * support for system calls is needed, the operations structure must be tagged
+ * with `__subsystem` and follow the `${class}_driver_api` naming scheme.
+ */
+
+/** @brief PN532 driver class operations */
+__subsystem struct pn532_driver_api {
+	/**
+	 * @brief Retrieve the firmware version of the PN532 module.
+	 *
+	 * This function queries the PN532 device for its firmware version.
+	 * The version is returned as a 32-bit value encoding IC version, 
+	 * firmware version, and revision.
+	 *
+	 * @param dev Pointer to the PN532 device instance.
+	 * @param version Pointer to a uint32_t where the firmware version will be stored.
+	 *
+	 * @retval 0 if successful.
+	 * @retval -EIO if communication with the device fails.
+	 * @retval -EINVAL if @p version is NULL.
+	 * @retval -errno Other negative errno codes on failure.
+	 */
+	int (*get_firmware_version)(const struct device *dev, uint32_t *version);
+};
+
+/** @} */
+
+/**
+ * @defgroup drivers_pn532_api PN532 driver API
+ * @{
+ *
+ * @brief Public API provided by the PN532 driver class.
+ *
+ * The public API defines the interface used by applications to interact with
+ * PN532 NFC devices. If support for system calls is required, API functions 
+ * must be annotated with `__syscall` and provide a corresponding implementation
+ * named `z_impl_<function_name>`, following Zephyr's syscall conventions.
+ */
+
+/**
+ * @brief Get the firmware version of the PN532 NFC module.
+ *
+ *
+ * @param dev Pointer to the PN532 device instance.
+ * @param version Pointer to a uint32_t where the firmware version will be stored.
+ *
+ * @retval 0 if successful.
+ * @retval -EIO if communication with the PN532 fails.
+ * @retval -EINVAL if @p version is NULL.
+ * @retval -errno Other negative errno codes on failure.
+ */
+__syscall int pn532_get_firmware_version(const struct device *dev, uint32_t *version);
+
+static inline int z_impl_pn532_get_firmware_version(const struct device *dev, uint32_t *version)
+{
+	__ASSERT_NO_MSG(DEVICE_API_IS(pn532, dev));
+
+	return DEVICE_API_GET(pn532, dev)->get_firmware_version(dev, version);
+}
+
+#include <syscalls/pn532.h>
+
+#endif /* PN532_H */
