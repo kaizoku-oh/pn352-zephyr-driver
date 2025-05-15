d0fb50fbc7286e94b26c5a6952d31268cc4e93cb
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-15 16:40:31 +0100

    Update cmake

--- tests/unit/src/main.c
+++ tests/unit/src/main.c
@@ -4,20 +4,20 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pn532_tests);
 
-#include "../../..include/zephyr/drivers/pn532/pn532.h"
+#include "pn532.h"
 
 ZTEST_SUITE(pn532_tests, NULL, NULL, NULL, NULL, NULL);
 
 /**
  * @brief Test firmware version retrieval for the PN532 driver
  */
-ZTEST(pn532_tests, test_driver)
+ZTEST(pn532_tests, test_get_firmware_version)
 {
     uint32_t version = 0;
     const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
 
-    zassert_equal(0, pn532_get_firmware_version(dev, &version));
-    zassert_equal(EINVAL, pn532_get_firmware_version(NULL, &version));
-    zassert_equal(EINVAL, pn532_get_firmware_version(dev, NULL));
     zassert_equal(EINVAL, pn532_get_firmware_version(NULL, NULL));
+    zassert_equal(EINVAL, pn532_get_firmware_version(dev, NULL));
+    zassert_equal(EINVAL, pn532_get_firmware_version(NULL, &version));
+    zassert_equal(0, pn532_get_firmware_version(dev, &version));
 }
