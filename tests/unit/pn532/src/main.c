624aaac6e8f777ac0e55106037d6bcc8f8f26e77
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-16 18:22:50 +0100

    Adding unit tests workflow (#3)

--- tests/unit/pn532/src/main.c
+++ tests/unit/pn532/src/main.c
@@ -6,17 +6,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/ztest.h>
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(pn532_tests);
 
 #include "pn532.h"
 
-ZTEST_SUITE(pn532_tests, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(pn532, NULL, NULL, NULL, NULL, NULL);
 
 /**
  * @brief Test firmware version retrieval for the PN532 driver
  */
-ZTEST(pn532_tests, test_get_firmware_version)
+ZTEST(pn532, test_get_firmware_version)
 {
     uint32_t version = 0;
     const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
