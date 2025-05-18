0fd55df3c71246742dada664fbe996b0f00e96a6
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-18 17:51:55 +0100

    Add workaround to avoid init driver when running tests

--- drivers/pn532/pn532.c
+++ drivers/pn532/pn532.c
@@ -38,7 +38,7 @@ static DEVICE_API(pn532, pn532_api) = {
     .pn532_get_firmware_version = &get_firmware_version,
 };
 
-#if DT_HAS_COMPAT_STATUS_OKAY(nxp_pn532, okay)
+#if DT_HAS_COMPAT_STATUS_OKAY(nxp_pn532)
 static int pn532_init(const struct device *dev)
 {
     int ret = pn532_transport_init(dev);
