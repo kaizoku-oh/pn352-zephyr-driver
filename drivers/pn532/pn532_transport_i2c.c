49e9fcca5b674ceb90f07c5daf28d6fee8a441e9
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-18 17:23:32 +0100

    Augh!!

--- drivers/pn532/pn532_transport_i2c.c
+++ drivers/pn532/pn532_transport_i2c.c
@@ -13,21 +13,21 @@ LOG_MODULE_REGISTER(pn532_i2c, CONFIG_PN532_LOG_LEVEL);
 
 int pn532_transport_init(const struct device *dev)
 {
-    LOG_INF("Mock PN532 transport initialized");
+    LOG_INF("PN532 I2C transport initialized");
 
     return 0;
 }
 
 int pn532_transport_write(const struct device *dev, const uint8_t *buf, size_t len)
 {
-    LOG_DBG("Mock write %zu bytes", len);
+    LOG_DBG("PN532 I2C write %zu bytes", len);
 
     return 0;
 }
 
 int pn532_transport_read(const struct device *dev, uint8_t *buf, size_t len)
 {
-    LOG_DBG("Mock read %zu bytes", len);
+    LOG_DBG("PN532 I2C read %zu bytes", len);
     memset(buf, 0xA5, len);
 
     return 0;
--- drivers/pn532/pn532_transport_mock.c
+++ drivers/pn532/pn532_transport_mock.c
@@ -5,27 +5,27 @@
 
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(pn532_test, CONFIG_PN532_LOG_LEVEL);
+LOG_MODULE_REGISTER(pn532_mock, CONFIG_PN532_LOG_LEVEL);
 
 #include "pn532_transport.h"
 
 int pn532_transport_init(const struct device *dev)
 {
-    LOG_INF("Mock PN532 transport initialized");
+    LOG_INF("PN532 mock transport initialized");
 
     return 0;
 }
 
 int pn532_transport_write(const struct device *dev, const uint8_t *buf, size_t len)
 {
-    LOG_DBG("Mock write %zu bytes", len);
+    LOG_DBG("PN532 mock write %zu bytes", len);
 
     return 0;
 }
 
 int pn532_transport_read(const struct device *dev, uint8_t *buf, size_t len)
 {
-    LOG_DBG("Mock read %zu bytes", len);
+    LOG_DBG("PN532 mock read %zu bytes", len);
     memset(buf, 0xA5, len);
 
     return 0;
