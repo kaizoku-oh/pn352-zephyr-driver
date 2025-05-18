53fad43ae3c13865ca2502da9dc5e435b56e7790
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2025-05-18 17:06:32 +0100

    Update test

--- tests/drivers/pn532/src/main.c
+++ tests/drivers/pn532/src/main.c
@@ -17,7 +17,7 @@ ZTEST_SUITE(pn532, NULL, NULL, NULL, NULL, NULL);
 ZTEST(pn532, test_get_firmware_version)
 {
     uint32_t version = 0;
-    const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
+    // const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
 
     zassert_equal(EINVAL, pn532_get_firmware_version(NULL, NULL));
     zassert_equal(0, version);
